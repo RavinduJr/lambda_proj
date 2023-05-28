d1a5d7fffde2275db28f889ab16287b1bc26f503
Ravindu Jayasekara <rav.t.jay97@gmail.com>
2023-05-28 16:30:08 +0530

    abstract methods implemented

---
+++ src/com/lambda/abstracts/Animal.java
@@ -0,0 +1,11 @@
+package com.lambda.abstracts;
+
+public abstract class Animal {
+
+	public abstract void animalSounds();
+
+	public void animalName(String name) {
+		System.out.println("Animal name is " + name);
+	}
+	
+}
--- src/com/lambda/main/Main.java
+++ src/com/lambda/main/Main.java
@@ -1,10 +1,27 @@
-package lambda_proj;
+package com.lambda.main;
+
+import com.lambda.abstracts.Animal;
+
+class Pig extends Animal {
+
+	@Override
+	public void animalSounds() {
+		System.out.println("Oink");
+		
+	}
+
+	
+
+}
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Pig pig = new Pig();
+		pig.animalName("pig");
+		pig.animalSounds();
 	}
 
 }
