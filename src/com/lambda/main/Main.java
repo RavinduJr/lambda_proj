4632eecda06178997b2608028376e02d926a26ea
Ravindu Jayasekara <rav.t.jay97@gmail.com>
2023-06-01 06:21:07 +0530

    Icalservice

--- src/com/lambda/main/Main.java
+++ src/com/lambda/main/Main.java
@@ -1,16 +1,25 @@
 package com.lambda.main;
 
 import com.lambda.abstracts.Animal;
+import com.lambda.interfaces.Calculations;
+import com.lambda.interfaces.ICalculateService;
 
 class Pig extends Animal {
 
 	@Override
 	public void animalSounds() {
 		System.out.println("Oink");
-		
+
 	}
 
-	
+}
+
+class Calculator implements Calculations {
+
+	@Override
+	public void calculateTotal() {
+		System.out.println("yo");
+	}
 
 }
 
@@ -19,6 +28,12 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Calculator c = new Calculator();
+		
+		c.calculateTotal();
+ICalculateService cal = () -> {
+	
+};
 		Pig pig = new Pig();
 		pig.animalName("pig");
 		pig.animalSounds();
